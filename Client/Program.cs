bc08f5cda6f306330281c6a573969c18054d6b8e
clement trompier <clem.trompier@gmail.com>
2023-11-15 08:59:47 +0100

    Ajoutez des fichiers projet.

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using R508_TP03_Blazor;
+
+namespace R508_TP03_Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
